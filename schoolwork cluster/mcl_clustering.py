d8c77b49fcf41e0d49c1f654dd31f8fd3cfff89c
杜羿萱-2016 <lqiamxu@sina.com>
2019-01-23 18:39:48 +0800

    Add files via upload

--- schoolwork cluster/mcl_clustering.py
+++ schoolwork cluster/mcl_clustering.py
@@ -4,7 +4,7 @@ import numpy as np
 import time
 from optparse import OptionParser
 import logging
-#构建邻接矩阵
+#构建G矩阵
 def get_graph(csv_filename):
     import networkx as nx
 
@@ -15,6 +15,25 @@ def get_graph(csv_filename):
 
     G = nx.from_numpy_matrix(np.matrix(M))
     return np.array(M), G
+#构建相似度矩阵
+from sklearn import datasets
+iris = datasets.load_iris()
+features = iris.data
+A =[]
+n=150
+
+def grapha(features):
+    n=150
+    A =[]
+    for i in features:
+        for j in features:
+            distance = np.abs(i[0]-j[0])+np.abs(i[1]-j[1])+np.abs(i[2]-j[2])+np.abs(i[3]-j[3])  
+            A.append(float(distance))  
+    A = np.array(A)
+    A = s_array.reshape(n,n) 
+    return A
+
+
 #标准化矩阵
 def normalize(A):
     column_sums = A.sum(axis=0)
@@ -26,10 +45,10 @@ def inflate(A, inflate_factor):
 #扩展矩阵
 def expand(A, expand_factor):
     return np.linalg.matrix_power(A, expand_factor)
-
+#自环
 def add_diag(A, mult_factor):
     return A + mult_factor * np.identity(A.shape[0])
-#markov聚类
+
 def get_clusters(A):
     clusters = []
     for i, r in enumerate((A>0).tolist()):
@@ -76,7 +95,7 @@ def stop(M, i):
 
     return False
 
-
+#马尔可夫聚类
 def mcl(M, expand_factor = 2, inflate_factor = 2, max_loop = 10 , mult_factor = 1):
     M = add_diag(M, mult_factor)
     M = normalize(M)
@@ -95,6 +114,58 @@ def networkx_mcl(G, expand_factor = 2, inflate_factor = 2, max_loop = 10 , mult_
     A = nx.adjacency_matrix(G)
     return mcl(np.array(A.todense()), expand_factor, inflate_factor, max_loop, mult_factor)
 
+def print_info(options):
+    print("-" * 60)
+    print("MARKOV CLUSTERING:")
+    print("-" * 60)
+    print("  expand_factor: %s" % options.expand_factor)
+    print("  inflate_factor: %s" % options.inflate_factor)
+    print("  mult factor: %s" % options.mult_factor)
+    print("  max loops: %s\n" % options.max_loop)
+
+def get_options():
+    usage = "usage: %prog [options] <input_matrix>"
+    parser = OptionParser(usage)
+    parser.add_option("-e", "--expand_factor",
+                      dest="expand_factor",
+                      default=2,
+                      type=int,
+                      help="expand factor (default: %default)")
+    parser.add_option("-i", "--inflate_factor",
+                      dest="inflate_factor",
+                      default=2,
+                      type=float,
+                      help="inflate factor (default: %default)")
+    parser.add_option("-m", "--mult_factor",
+                      dest="mult_factor",
+                      default=2,
+                      type=float,
+                      help="multiply factor (default: %default)")
+    parser.add_option("-l", "--max_loops",
+                      dest="max_loop",
+                      default=60,
+                      type=int,
+                      help="max loops (default: %default)")
+    parser.add_option("-o", "--output", metavar="FILE", 
+                      help="output (default: stdout)")
+
+    parser.add_option("-v", "--verbose",
+                      action="store_true", dest="verbose", default=True,
+                      help="verbose (default: %default)")
+    parser.add_option("-d", "--draw-graph",
+                      action="store_true", dest="draw", default=False,
+                      help="show graph with networkx (default: %default)")
+    
+
+    (options, args) = parser.parse_args()
+
+    try:
+        filename = args[0]
+    except:
+        raise Exception('input', 'missing input filename')
+
+
+    return options, filename
 #聚类的输出
 def clusters_to_output(clusters, options):
     if options.output and len(options.output)>0:
@@ -106,6 +177,7 @@ def clusters_to_output(clusters, options):
         print("Clusters:")
         for k, v in clusters.items():
             print('{}, {}'.format(k, v))
+
 
 if __name__ == '__main__':
     M, G = get_graph(filename)
@@ -141,13 +213,13 @@ features = iris.data
 from sklearn.decomposition import PCA
 pca = PCA(n_components=2)
 pca.fit(features)
-irisdata= pca.transform(features)
+pca_features= pca.transform(features)
 #可视化原始iris数据集
-features_1 = plt.scatter(irisdata[0:50, 0], irisdata[0:50, 1],marker='o')
-features_2 = plt.scatter(irisdata[51:100, 0], irisdata[51:100, 1],marker='ve')
-features_3 = plt.scatter(irisdata[101:149, 0], irisdata[101:149, 1],marker='vi')
+features_1 = plt.scatter(pca_features[0:50, 0], pca_features[0:50, 1],marker='o')
+features_2 = plt.scatter(pca_features[51:100, 0], pca_features[51:100, 1],marker='ve')
+features_3 = plt.scatter(pca_features[101:149, 0], pca_features[101:149, 1],marker='vi')
 plt.legend(handles=[features_1, features_2,features_3], labels=['setosa', 'versicolor', 'virginica'],  loc='best')
 plt.plot()
 #对原始数据集进行mcl聚类
-M, clusters = networkx_mcl(irisdata)
+M, clusters = networkx_mcl(features)
 
